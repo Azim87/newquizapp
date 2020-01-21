6cd7bdcf6f82be35d23017b35c9716fbedae189c
Azimus <millennium1313>
2020-01-21 23:19:52 +0600

    show count result in settings fragment

---
+++ app/src/main/java/com/example/quizapp/base/BaseFragment.java
@@ -0,0 +1,30 @@
+package com.example.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    @LayoutRes
+    protected abstract int getLayoutResourceId();
+    protected abstract void setUpView(View view);
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getLayoutResourceId(), container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUpView(view);
+    }
+}

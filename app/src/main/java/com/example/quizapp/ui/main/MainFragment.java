6af1a1d027e41ae2e0c554b60c4d6c1f9c6f5f59
Azimus <millennium1313>
2020-01-30 11:48:55 +0600

    added simple seekbar change listener

--- app/src/main/java/com/example/quizapp/ui/main/MainFragment.java
+++ app/src/main/java/com/example/quizapp/ui/main/MainFragment.java
@@ -1,15 +1,31 @@
 package com.example.quizapp.ui.main;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.quizapp.R;
 import com.example.quizapp.base.BaseFragment;
+import com.example.quizapp.utils.SimpleSeekBarChangeListener;
+
+import org.angmarch.views.NiceSpinner;
+
+import butterknife.BindView;
 
 public class MainFragment extends BaseFragment {
+    @BindView(R.id.amount_seekbar)
+    SeekBar seekBar;
+    @BindView(R.id.category_spinner)
+    NiceSpinner categorySpinner;
+    @BindView(R.id.difficulty_spinner)
+    NiceSpinner difficultySpinner;
+    @BindView(R.id.question_amount_quantity)
+    TextView questionAmount;
 
     private MainViewModel mainViewModel;
 
@@ -24,6 +40,17 @@ public class MainFragment extends BaseFragment {
 
     @Override
     protected void setUpView(View view) {
+        questionAmount.setText(String.valueOf(seekBar.getProgress()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            seekBar.setMin(5);
+        }
+        seekBar.setOnSeekBarChangeListener(new SimpleSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                super.onProgressChanged(seekBar, progress, fromUser);
+                questionAmount.setText(String.valueOf(progress));
+            }
+        });
     }
 
     @Override

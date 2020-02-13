375178bf1eb9af31bb8b920405637eb8a0605c3d
Azimus <millennium1313>
2020-02-13 17:39:54 +0600

    delete quiz history by id by swiping it

--- app/src/main/java/com/example/quizapp/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/quizapp/ui/history/HistoryFragment.java
@@ -56,6 +56,22 @@ public class HistoryFragment extends BaseFragment
         historyRecycler.setLayoutManager(
                 new LinearLayoutManager(getContext()));
         historyRecycler.setAdapter(adapter);
+        deleteByIdOnSwipe();
+    }
+
+    private void deleteByIdOnSwipe() {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteById(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(historyRecycler);
     }
 
     @Override
